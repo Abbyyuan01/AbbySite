4b00393f23d415add7f71bbea4dabda761e37f0a
Abbyyuan <abbyyuan01@gmail.com>
2020-03-07 16:14:10 +1100

    add View

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import Sidebar from './components/sidebar'
+import Introduction from './components/introduction'
+import About from './components/about'
+import Timeline from './components/timeline'
+import Blog from './components/blog'
+import Projects from './components/projects'
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="colorlib-page">
+      <div id="container-wrap">
+        <Sidebar></Sidebar>
+      <div id="colorlib-main">
+        <Introduction></Introduction>
+        <About></About>
+        <Timeline></Timeline>
+        <Blog/>
+        <Projects/>
+          </div>
+      </div>
     </div>
   );
 }
