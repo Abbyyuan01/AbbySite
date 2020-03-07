087265234aba52b277a66e95960b6cb3e5979be7
Abbyyuan <abbyyuan01@gmail.com>
2020-03-07 16:57:33 +1100

    change content

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -1,51 +1,65 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Introduction extends Component {
   render() {
     return (
       <div>
-        <section id="colorlib-hero" className="js-fullheight" data-section="home">
+        <section
+          id="colorlib-hero"
+          className="js-fullheight"
+          data-section="home"
+        >
           <div className="flexslider js-fullheight">
             <ul className="slides">
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+              <li style={{ backgroundImage: "url(images/img_bg.jpg)" }}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
-                          <h1>Hi! <br />I'm Dhruv</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://docs.google.com/document/d/15coSa2rnZcdywihM-pWTvoGqE65JAkxQAh6JDBNrF2Q/edit?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                          <h1>
+                            Hi! <br />
+                            I'm Yuan Yuan
+                          </h1>
+                          <p>
+                            <a
+                              className="btn btn-primary btn-learn"
+                              href="https://drive.google.com/file/d/1zYXU8w8ZQC4G50wBxqohaADX6ob9pWcm/view?usp=sharing"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              View CV
+                              <i className="icon-download4" />
+                            </a>
+                          </p>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </li>
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+              <li style={{ backgroundImage: "url(images/img_bg.jpg)" }}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>I love building<br /> THINGS !!</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://github.com/Dhruv34788" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
-                <div className="overlay" />
-                <div className="container-fluid">
-                  <div className="row">
-                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                      <div className="slider-text-inner">
-                        <div className="desc">
-                          <h1>I often <br/>Write ... </h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://medium.com/@dhruv.barochia34788" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
+                          <h1>
+                            I love building
+                            <br /> THINGS !!
+                          </h1>
+                          <p>
+                            <a
+                              className="btn btn-primary btn-learn"
+                              href="https://github.com/Abbyyuan01"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              View Projects <i className="icon-briefcase3" />
+                            </a>
+                          </p>
                         </div>
                       </div>
                     </div>
@@ -56,6 +70,6 @@ export default class Introduction extends Component {
           </div>
         </section>
       </div>
-    )
+    );
   }
 }
